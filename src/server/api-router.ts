5a6c005490edadb922481929abffdab9de14ab04
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2023-06-15 14:32:45 +1000

    data persistence... adding a new name to a content item

--- src/server/api-router.ts
+++ src/server/api-router.ts
@@ -5,6 +5,7 @@ import { connectClient } from "./db";
 
 const router = express.Router();
 router.use(cors());
+router.use(express.json());
 
 router.get("/contests", async (req, res) => {
   const client = await connectClient();
@@ -29,6 +30,49 @@ router.get("/contest/:contestId", async (req, res) => {
   const contest = await client
     .collection("contests")
     .findOne({ id: req.params.contestId });
+
+  res.send({ contest });
+});
+
+router.post("/contest/:contestId", async (req, res) => {
+  const client = await connectClient();
+
+  const { newNameValue } = req.body;
+
+  const doc = await client
+    .collection("contests")
+    .findOneAndUpdate(
+      { id: req.params.contestId },
+      {
+        $push: {
+          names: {
+            id: newNameValue.toLowerCase().replace(/\s/g, "-"),
+            name: newNameValue,
+            timestamp: new Date(),
+          },
+        },
+      },
+      { returnDocument: "after" },
+    );
+
+  res.send({ updatedContest: doc.value });
+});
+
+router.post("/contests/", async (req, res) => {
+  const { contestName, categoryName, description } = req.body;
+
+  const client = await connectClient();
+  const doc = await client.collection("contests").insertOne({
+    id: contestName.toLowerCase().replace(/\s/g, "-"),
+    contestName,
+    categoryName,
+    description,
+    names: [],
+  });
+
+  const contest = await client
+    .collection("contests")
+    .findOne({ _id: doc.insertedId });
 
   res.send({ contest });
 });
