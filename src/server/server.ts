970ad162e888895d9d876aee230eaaba8bd27a5c
negro-vinilo <52874360+negro-vinilo@users.noreply.github.com>
2023-06-15 12:37:50 +1000

    [npm packages/webpack/api(s)/config/server] mongodb connection via api's using client & server side api calls; axios & cors packages added/jsx components created for list and preview and global variable added to remove unnecessary duplicate fetch call to 'contests' data

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import os from "node:os";
 
 import config from "./config";
+import apiRouter from "./api-router";
+import serverRender from "./render";
 
 const server = express();
 
@@ -9,15 +11,19 @@ server.use(express.static("dist"));
 
 server.set("view engine", "ejs");
 
-server.use("/", (req, res) => {
+server.use("/api", apiRouter);
+
+server.get("/", async (req, res) => {
+  const { initialMarkup, initialData } = await serverRender();
   res.render("index", {
-    initialContent: "Loading...",
+    initialMarkup,
+    initialData,
   });
 });
 
 server.listen(config.PORT, config.HOST, () => {
-  console.info(
-    `Express server is listening at ${config.SERVER_URL}`,
-    `Free Men: ${os.freemem() / 1024 / 1024}`,
+  console.log(
+    `Express is listening at ${config.SERVER_URL}`,
+    `Free Mem: ${os.freemem() / 1024 / 1024}`,
   );
 });
